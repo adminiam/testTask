22564c46a19df30ac12ad0bdd6a489933c634df3
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2024-10-01 09:52:13 +0300

    testTask

--- src/main/java/org/example/DocumentManager.java
+++ src/main/java/org/example/DocumentManager.java
@@ -4,55 +4,47 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.Instant;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
-/**
- * For implement this task focus on clear code, and make this solution as simple readable as possible
- * Don't worry about performance, concurrency, etc
- * You can use in Memory collection for sore data
- * <p>
- * Please, don't change class name, and signature for methods save, search, findById
- * Implementations should be in a single class
- * This class could be auto tested
- */
 public class DocumentManager {
 
-    /**
-     * Implementation of this method should upsert the document to your storage
-     * And generate unique id if it does not exist, don't change [created] field
-     *
-     * @param document - document content and author data
-     * @return saved document
-     */
+    private final Map<String, Document> storage = new HashMap<>();
+
     public Document save(Document document) {
-
-        return null;
+        if (document.getId() == null || document.getId().isEmpty()) {
+            document.setId(UUID.randomUUID().toString());
+        }
+        storage.put(document.getId(), document);
+        return document;
     }
 
-    /**
-     * Implementation this method should find documents which match with request
-     *
-     * @param request - search request, each field could be null
-     * @return list matched documents
-     */
     public List<Document> search(SearchRequest request) {
-
-        return Collections.emptyList();
+        return storage.values().stream().filter(document -> match(document,request)).collect(Collectors.toList());
     }
 
-    /**
-     * Implementation this method should find document by id
-     *
-     * @param id - document id
-     * @return optional document
-     */
     public Optional<Document> findById(String id) {
 
-        return Optional.empty();
+        return Optional.ofNullable(storage.get(id));
     }
 
+    private boolean match(Document document, SearchRequest request) {
+        if (request.getTitlePrefixes() != null && request.getTitlePrefixes().stream()
+                .noneMatch(prefix -> document.getTitle().startsWith(prefix))) {
+            return false;
+        }
+        if (request.getContainsContents() != null && request.getContainsContents().stream()
+                .noneMatch(content -> document.getContent().contains(content))) {
+            return false;
+        }
+        if (request.getAuthorIds() != null && !request.getAuthorIds().contains(document.getAuthor().getId())) {
+            return false;
+        }
+        if (request.getCreatedFrom() != null && document.getCreated().isBefore(request.getCreatedFrom())) {
+            return false;
+        }
+        return request.getCreatedTo() == null || !document.getCreated().isAfter(request.getCreatedTo());
+    }
     @Data
     @Builder
     public static class SearchRequest {
